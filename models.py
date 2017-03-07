2f642c1ad311437aa64e8ebc199c3048bacc3b22
Denis Tereshkin <denis@kasan.ws>
2017-03-07 20:13:19 +0700

    ClosedTrade model

--- models.py
+++ models.py
@@ -16,3 +16,18 @@ class Trade(models.Model):
     timestamp = models.DateTimeField()
     balanced = models.BooleanField(default=False)
 
+class ClosedTrade(models.Model):
+    LONG = 'long'
+    SHORT = 'short'
+    DIRECTION_CHOICES = (
+                (LONG, 'long'),
+                (SHORT, 'short')
+            )
+    account = models.CharField(max_length=256)
+    security = models.CharField(max_length=256)
+    entryTime = models.DateTimeField()
+    exitTime = models.DateTimeField()
+    profit = models.DecimalField(max_digits=25, decimal_places=10)
+    profitCurrency = models.CharField(max_length=10)
+    strategyId = models.CharField(max_length=64)
+    direction = models.CharField(max_length=10, choices=DIRECTION_CHOICES)
